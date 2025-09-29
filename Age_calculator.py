5601346c7f9cc42eeed9568ed5e8cc63efcd82b8
“[firstname <saishwarya23@tbc.edu.np>
2025-09-29 23:39:49 +0545

    files added

---
+++ Age_calculator.py
@@ -0,0 +1,14 @@
+
+#Simple Age Calculator
+# Take input from user
+birthyear = int(input("Enter your birth year: "))
+
+# Current year 
+currentyear = 2025  
+
+# Calculate age
+age = currentyear - birthyear
+
+print(f"You are {age} years old.")
+
+
---
+++ Basics.py
@@ -0,0 +1,21 @@
+#print
+print("Learning Utsav2025")
+
+#variables and data types
+num1 = 7
+num2 = 2.33
+name = "Aishwarya"
+
+
+print(name)
+print(num2)
+
+
+#type of variable
+print(type(num2))
+
+#sum of two numbers
+calculate = num1 + num2 
+print(calculate)
+
+
