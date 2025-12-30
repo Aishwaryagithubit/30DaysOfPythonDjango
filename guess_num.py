55dc54d904e04d0024c0625c0bf923425fea820e
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-12-30 22:11:33 +0545

    file added

---
+++ guess_num.py
@@ -0,0 +1,14 @@
+import random
+secret_number = random.randint(1, 100)
+
+print("Welcome to the Number Guessing Game")
+while True:
+    guess = int(input("Enter your guess: "))
+    
+    if guess < secret_number:
+        print("Too low,Try again.")
+    elif guess > secret_number:
+        print("Too high,Try again.")
+    else:
+        print("Congratulations! You guessed the number correctly")
+        break
